2ae59fdf47ddbf9248d6f0600a9d35a6d4886c2f
AlinaLepekhina <alina.lepekhina.07@mail.ru>
2025-10-24 00:51:25 +0300

    Add files via upload

---
+++ лаба 3 программирование Лепехина.py номер 4.py
@@ -0,0 +1,37 @@
+"""a = int(input())
+if a % 400 == 0 or a % 4 == 0 and a % 100 != 0
+    print("YES")
+else:
+    print("NO")"""
+
+
+"""a = int(input())
+if a % 400 == 0:
+    print("yes")
+else:
+    if a % 100 == 0:
+        print("no")
+    else:
+        if a % 4 == 0:
+            print("yes")
+        else:
+            print("no")"""
+
+
+a = int(input())
+if a % 400 == 0:
+    print ("yes")
+elif a % 100 == 0:
+    print("no")
+elif a % 4 == 0:
+    print("yes")
+else:
+    print("no")
+
+    
+
+
+
+
+
+
